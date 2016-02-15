d5ca3f8db94442f30ae47ffecf3312a00a046fca
Evol Johnson <ejohnson@ngeosone.com>
2016-02-15 01:15:12 -0500

    project structure

---
+++ HomeworkBackend/HomeworkBackend.h
@@ -0,0 +1,19 @@
+//
+//  HomeworkBackend.h
+//  HomeworkBackend
+//
+//  Created by Evol M Johnson on 2/15/16.
+//  Copyright © 2016 NgeosOne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HomeworkBackend.
+FOUNDATION_EXPORT double HomeworkBackendVersionNumber;
+
+//! Project version string for HomeworkBackend.
+FOUNDATION_EXPORT const unsigned char HomeworkBackendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HomeworkBackend/PublicHeader.h>
+
+
